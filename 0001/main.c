79c7d8c31657bc3408bcfda182b5a822364566d9
Hao Yan <yanhao29@gmail.com>
2015-09-29 07:59:29 -0700

    Initial Commit

---
+++ 0001/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  0001
+//
+//  Created by Hao Yan on 9/29/15.
+//  Copyright © 2015 Hao Yan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
